b3c466383768e8c59d43a716e93e7f0596e99f46
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2023-01-17 16:32:48 +0100

    update to angular v11

--- src/app/pages/sidebar-free/sidebar-free.component.spec.ts
+++ src/app/pages/sidebar-free/sidebar-free.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SidebarFreeComponent } from './sidebar-free.component';
 
@@ -6,7 +6,7 @@ describe('SidebarFreeComponent', () => {
   let component: SidebarFreeComponent;
   let fixture: ComponentFixture<SidebarFreeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SidebarFreeComponent ]
     })
